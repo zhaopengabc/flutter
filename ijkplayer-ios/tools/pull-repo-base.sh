01e89ee27b082e09f1a224c4107cb61969d4b84a
zhaopeng <zhaopenggong@163.com>
2020-07-28 14:02:53 +0800

    add ijkplayer-ios

---
+++ ijkplayer-ios/tools/pull-repo-base.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+REMOTE_REPO=$1
+LOCAL_WORKSPACE=$2
+
+
+if [ -z $REMOTE_REPO -o -z $LOCAL_WORKSPACE ]; then
+    echo "invalid call pull-repo.sh '$REMOTE_REPO' '$LOCAL_WORKSPACE'"
+elif [ ! -d $LOCAL_WORKSPACE ]; then
+
+    echo "========================="
+    echo "git clone $REMOTE_REPO $LOCAL_WORKSPACE"
+    git clone $REMOTE_REPO $LOCAL_WORKSPACE
+else
+
+    echo "------------------------------------"
+    cd $LOCAL_WORKSPACE
+    git fetch --all --tags
+    cd -
+fi
