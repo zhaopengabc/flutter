c8b4a1873ba264e9f9b97b6d4956755d449a0a39
zhaopeng <zhaopenggong@163.com>
2020-08-12 16:23:27 +0800

    add soft decode

--- flutter_ijkplayer_source/ijkmedia/ijkplayer/ff_ffplay.c
+++ flutter_ijkplayer_source/ijkmedia/ijkplayer/ff_ffplay.c
@@ -567,7 +567,7 @@ fail0:
 
     return ret;
 }
-/*
+
 //add for flutter
 #if !defined (__ANDROID__)
 static int copyAVFrameToPixelBuffer(FFPlayer *ffp, AVCodecParameters* avcp, const AVFrame* frame, CVPixelBufferRef cv_img, const size_t* plane_strides, const size_t* plane_rows)
@@ -664,7 +664,7 @@ int createCVPixelBuffer(FFPlayer *ffp, AVCodecParameters* avcp, AVFrame* frame, 
 }
 #endif
 //end
-*/
+
 static int decoder_decode_frame(FFPlayer *ffp, Decoder *d, AVFrame *frame, AVSubtitle *sub) {
     int ret = AVERROR(EAGAIN);
 
@@ -686,7 +686,7 @@ static int decoder_decode_frame(FFPlayer *ffp, Decoder *d, AVFrame *frame, AVSub
                             } else if (!ffp->decoder_reorder_pts) {
                                 frame->pts = frame->pkt_dts;
                             }
-                            /*
+                            
                             //add for flutter
                              #if !defined (__ANDROID__)
                             ffp_pixelbuffer_lock(ffp);
@@ -704,7 +704,7 @@ static int decoder_decode_frame(FFPlayer *ffp, Decoder *d, AVFrame *frame, AVSub
                             ffp_pixelbuffer_unlock(ffp);
                             #endif
                             //add end
-                            */
+                            
                         }
                         break;
                     case AVMEDIA_TYPE_AUDIO:
